7e5f5ac8ccffcff6b427e1181f84e3cfe301fa1d
Shahzaib <shahzjee@gmail.com>
2024-03-04 23:19:46 +0500

    first assignment

---
+++ main.js
@@ -0,0 +1,6 @@
+"use strict";
+// assignment # 1
+// storing the person name in a variabe
+let personname = "Shahzaib";
+//Printing the message to the person
+console.log(`Hello ${personname} would you like to learn some Python today?`);
---
+++ main.ts
@@ -0,0 +1,5 @@
+// assignment # 1
+// storing the person name in a variabe
+let personname:string = "Shahzaib"
+//Printing the message to the person
+console.log(`Hello ${personname} would you like to learn some Python today?`)
