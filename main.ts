e037997cf4e62026780816f1d3063f5440ed5c9f
Shahzaib <shahzjee@gmail.com>
2024-03-07 09:29:06 +0500

    first commit

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
-// assignment # 1
-// storing the person name in a variabe
+/* assignment # 1
+storing the person name in a variabe*/
 let personname:string = "Shahzaib"
 //Printing the message to the person
 console.log(`Hello ${personname} would you like to learn some Python today?`)
